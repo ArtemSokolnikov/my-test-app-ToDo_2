c153481709cecd3ebc57417a77e0b3d90bc686b6
Artem Sokolnikov <s9634331017@yandex.ru>
2023-06-13 10:59:30 +0300

    first_commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const targetWord = 'HELLO';
+    const [guess, setGuess] = useState('');
+    const [attempts, setAttempts] = useState(0);
+    const [keyboard, setKeyboard] = useState({});
+
+    useEffect(() => {
+        setKeyboard(getInitialKeyboardState());
+    }, []);
+
+    const getInitialKeyboardState = () => {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return alphabet.split('').reduce((keyboardState, letter) => {
+            keyboardState[letter] = false;
+            return keyboardState;
+        }, {});
+    };
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    const handleKeyDown = (event) => {
+        const { key } = event;
+        if (key.match(/^[a-zA-Z]$/)) {
+            setGuess((prevGuess) => prevGuess + key.toUpperCase());
+            setKeyboard((prevKeyboard) => ({
+                ...prevKeyboard,
+                [key.toUpperCase()]: true,
+            }));
+        }
+    };
+
+    const handleGuess = (event) => {
+        event.preventDefault();
+        if (guess.length !== 5) {
+            alert('Введите слово из 5 букв');
+            return;
+        }
+        setAttempts(attempts + 1);
+        setGuess('');
+        setKeyboard(getInitialKeyboardState());
+    };
+
+    const getLetterColor = (letter) => {
+        if (targetWord.includes(letter)) {
+            return 'green';
+        } else {
+            return 'red';
+        }
+    };
+
+    return (
+        <div>
+            <h1>Wordle</h1>
+            <p>Попытка: {attempts}</p>
+            <p>Введите слово из 5 букв:</p>
+            <form onSubmit={handleGuess}>
+                <input
+                    type="text"
+                    value={guess}
+                    maxLength={5}
+                    onChange={(e) => setGuess(e.target.value.toUpperCase())}
+                />
+                <button type="submit">Проверить</button>
+            </form>
+            <div>
+                {Array.from(guess).map((letter, index) => (
+                    <span key={index} style={{ color: getLetterColor(letter) }}>
+            {letter}
+          </span>
+                ))}
+            </div>
+            <div>
+                {Object.keys(keyboard).map((letter) => (
+                    <span
+                        key={letter}
+                        style={{ color: keyboard[letter] ? 'blue' : 'black' }}
+                    >
+            {letter}
+          </span>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
